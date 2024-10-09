09b5ed5a70a50fbc81ddfdd44a53f1d1280e1880
Ludovico Pestarino <ludovico@pestarino.xyz>
2024-10-09 13:21:19 +0200

    new tests

--- tests/test_oracle_db_client.py
+++ tests/test_oracle_db_client.py
@@ -1,8 +1,7 @@
 import unittest
-from unittest.mock import patch, MagicMock
-# Replace 'your_module' with the actual module name where OracleDbClient is located
+from sqlalchemy import Engine
 from src.arpaletl.OracleDbClient import OracleDbClient
-
+from src.arpaletl.IDbClient import DbClientError
 
 class TestOracleDbClient(unittest.TestCase):
     """
@@ -10,70 +9,35 @@ class TestOracleDbClient(unittest.TestCase):
     """
 
 
-    @patch('oracledb.connect')  # Mock the oracledb.connect method
-    # Mock the sqlalchemy.create_engine method
-    @patch('sqlalchemy.create_engine')
-    @patch('os.getenv')  # Mock the os.getenv function
-    def test_oracle_db_client_init_success(self, mock_getenv, mock_create_engine, mock_connect):
+    def test_oracle_db_client_init_success(self):
         """
         Test the constructor of OracleDbClient
         """
-        # Set up the environment variables to return when called
-        mock_getenv.side_effect = ['test_user', 'test_password', 'test_dsn']
-
-        # Create a mock connection object
-        mock_connection = MagicMock()
-        mock_connect.return_value = mock_connection
-
-        # Create an instance of OracleDbClient
-        client = OracleDbClient()
-
-        # Assertions
-        mock_connect.assert_called_once_with(
-            user='test_user',
-            password='test_password',
-            dsn='test_dsn'
-        )
-        self.assertIsNotNone(client.conn)
-        self.assertEqual(client.conn, mock_connection)
-        self.assertIsNotNone(client.cursor)
-        mock_create_engine.assert_called_once()
+        db = OracleDbClient("test_user", "test_password", "test_dsn")
+        self.assertIsInstance(db.engine, Engine)
 
 
-    @patch('oracledb.connect')
-    @patch('os.getenv')
-    def test_oracle_db_client_init_failure(self, mock_getenv, mock_connect):
+    def test_oracle_db_client_init_failure(self):
         """
-        Test the constructor of OracleDbClient when the connection fails
+        Test the constructor of OracleDbClient with malformed credentials
         """
-        # Simulate a failure in getting environment variables
-        mock_getenv.side_effect = [None, None, None]
-
-        # Capture the SystemExit exception
-        with self.assertRaises(SystemExit) as cm:
-            OracleDbClient()
-
-        # Verify that the exit code is 1
-        self.assertEqual(cm.exception.code, 1)  # Ensure the exit code is 1
+        with self.assertRaises(DbClientError):
+            OracleDbClient("test_user", None, "test_dsn")
 
 
-    @patch('oracledb.connect')
-    @patch('sqlalchemy.create_engine')
-    @patch('os.getenv')
-    def test_oracle_db_client_engine_creation_failure(self, mock_getenv, mock_create_engine, mock_connect):
+    def test_oracle_db_client_connect_success(self):
         """
-        Test the constructor of OracleDbClient when the engine creation fails
+        Test the connect method of OracleDbClient
         """
-        mock_getenv.side_effect = ['test_user', 'test_password', 'test_dsn']
-        mock_connection = MagicMock()
-        mock_connect.return_value = mock_connection
 
-        # Simulate a failure in engine creation
-        mock_create_engine.side_effect = Exception("Engine creation failed")
 
-        # Expecting exit due to engine creation failure
-        with self.assertRaises(SystemExit):
-            OracleDbClient()
+    def test_oracle_db_client_connect_failure(self):
+        """
+        Test the connect method of OracleDbClient with invalid credentials
+        """
+        db = OracleDbClient("test_user", "test_password", "test_dsn")
+        with self.assertRaises(DbClientError):
+            db.connect()
 
 
 if __name__ == '__main__':
