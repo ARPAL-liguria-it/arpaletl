fc7ed16f618f243f776e3adc3fefb8c8f99151ad
Ludovico Pestarino <ludovico@pestarino.xyz>
2024-10-10 10:18:02 +0200

    added _async_open_stream

--- src/arpaletl/WebResource.py
+++ src/arpaletl/WebResource.py
@@ -1,6 +1,9 @@
+from typing import AsyncIterator
 import requests
+import asyncio
+import aiohttp
 from src.arpaletl.IResource import IResource, ResourceError
-from typing import Iterator
+
 
 class WebResource(IResource):
     """
@@ -16,7 +19,6 @@ class WebResource(IResource):
         self.timeout = timeout
         self.headers = headers
 
-
     def open(self) -> requests.Response:
         """
         Open method for WebResource it will download the entire 
@@ -24,20 +26,38 @@ class WebResource(IResource):
         @returns: Opened web resource that can be readed with read()
         """
         try:
-            r = requests.get(self.uri, timeout=self.timeout, headers=self.headers)
+            r = requests.get(self.uri, timeout=self.timeout,
+                             headers=self.headers)
             r.raise_for_status()
         except requests.exceptions.RequestException as e:
             raise ResourceError("Error downloading web resource") from e
         return r
 
-    def open_stream(self, chunk: int) -> Iterator:
+    def open_stream(self, chunk: int) -> AsyncIterator:
         """
         Open method for WebResource
         @returns: an Iterator that can be parsed in @chunk sized chunks
         """
         try:
-            r = requests.get(self.uri, timeout=self.timeout, headers=self.headers, stream=True)
+            r = requests.get(self.uri, timeout=self.timeout,
+                             headers=self.headers, stream=True)
             r.raise_for_status()
         except requests.exceptions.RequestException as e:
             raise ResourceError("Error downloading web resource") from e
         return r.iter_content(chunk_size=chunk)
+
+    async def _async_open_stream(self, chunk: int) -> AsyncIterator:
+        """
+        Async Open method for WebResource
+        @returns: an Iterator that can be parsed in @chunk sized chunks
+        """
+        try:
+            async with aiohttp.ClientSession() as session:
+                async with session.get(self.uri,
+                                       timeout=self.timeout,
+                                       headers=self.headers) as response:
+                    response.raise_for_status()
+                    async for data in response.content.iter_any(chunk):
+                        yield data
+        except aiohttp.ClientError as e:
+            raise ResourceError("Error downloading web resource") from e
