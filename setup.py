97227b5bbc2ecfd14a2fd12aa73afd9b148ca23a
Ludovico Pestarino <ludovico@pestarino.xyz>
2024-10-24 13:33:51 +0200

    added requirements

--- setup.py
+++ setup.py
@@ -3,6 +3,14 @@ from setuptools import find_packages, setup
 setup(
     name="arpaletl",
     packages=find_packages(),
-    version="0.0.1",
+    version="0.1.0-rc1",
     description="ARPAL ETL library",
+    install_requires=[    
+        'sqlalchemy',
+        'oracledb',
+        'pytest',
+        'coverage',
+        'pandas',
+        'aiohttp'
+    ],
 )
